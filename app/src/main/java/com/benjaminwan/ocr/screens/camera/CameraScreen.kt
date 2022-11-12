0015734940d06fe9c11dabdb746bb910c583ebb7
benjaminwan <benjaminwan@163.com>
2022-11-12 11:43:12 +0800

    空页面

--- app/src/main/java/com/benjaminwan/ocr/screens/camera/CameraScreen.kt
+++ app/src/main/java/com/benjaminwan/ocr/screens/camera/CameraScreen.kt
@@ -1,9 +1,26 @@
 package com.benjaminwan.ocr.screens.camera
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.benjaminwan.ocr.screens.CommonScaffold
+import com.benjaminwan.ocr.screens.NavBackIcon
+import com.benjaminwan.ocr.screens.Screen
 
 @Composable
 fun CameraScreen(navController: NavHostController) {
-
+    CommonScaffold(
+        modifier = Modifier.fillMaxSize(),
+        title = { Text(text = stringResource(id = Screen.Camera.titleId)) },
+        navigationIcon = { NavBackIcon(navController) },
+    ) { contentPadding ->
+        Column(modifier = Modifier.padding(contentPadding)) {
+            Text(text = "开发中，待后续版本更新……")
+        }
+    }
 }
